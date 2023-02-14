5a05ea911d195b6535a62aee0cbb0e86257dacbe
Talkasha <nivekzin@gmail.com>
2023-02-14 09:13:26 +0000

    feat(homePage): Implemented the home page that will hold menu and the articles

---
+++ src/pages/home/home-page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "semantic-ui-react";
+
+const HomePage = () => {
+
+    return (
+        <Container>
+            Text
+        </Container>
+    )
+}
+
+export default HomePage;
