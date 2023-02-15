c7ea5b83ebcc39e5ef6441dad8cf4cfb6548d745
Talkasha <nivekzin@gmail.com>
2023-02-15 06:50:23 +0000

    feat(auth): Implemented authenticated menu for menu-header

---
+++ src/components/menu-header/login-menu/authenticated-menu/authenticated-menu.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useAuth0, User } from "@auth0/auth0-react";
+import { Icon, Menu } from "semantic-ui-react";
+
+type AuthenticatedMenuProps = {
+    user: User | undefined
+}
+
+const AuthenticatedMenu = ({ user }: AuthenticatedMenuProps) => {
+
+    useEffect(() => {}, [])
+
+    return (
+        <Menu.Item
+            name="User"
+        >
+            <Icon name="user" />
+            {user?.name}
+        </Menu.Item>
+    )
+}
+
+export default AuthenticatedMenu;
---
+++ src/components/menu-header/login-menu/authenticated-menu/index.ts
@@ -0,0 +1,1 @@
+export { default as AuthenticatedMenu } from './authenticated-menu';
