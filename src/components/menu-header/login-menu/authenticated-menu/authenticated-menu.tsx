7b0fa571a818cd87e318457b5847a22dc9bf7487
Talkasha <nivekzin@gmail.com>
2023-02-16 05:22:29 +0000

    feat(menu): Updated authenticated menu with greetings and bookmark and account menu options (no redirection)

--- src/components/menu-header/login-menu/authenticated-menu/authenticated-menu.tsx
+++ src/components/menu-header/login-menu/authenticated-menu/authenticated-menu.tsx
@@ -7,16 +7,40 @@ type AuthenticatedMenuProps = {
 }
 
 const AuthenticatedMenu = ({ user }: AuthenticatedMenuProps) => {
+    const { logout } = useAuth0()
 
-    useEffect(() => {}, [])
+    useEffect(() => {
+        console.log(user)
+    }, [])
 
     return (
-        <Menu.Item
-            name="User"
-        >
-            <Icon name="user" />
-            Hello, {user?.name}
-        </Menu.Item>
+        <>
+            <Menu.Item
+                name="User"
+            >
+                <Icon name="smile outline" />
+                Hello, {user?.name}
+            </Menu.Item>
+            <Menu.Item
+                name="Bookmarks"
+            >
+                <Icon name="bookmark" />
+                My Bookmarks
+            </Menu.Item>
+            <Menu.Item
+                name="Account"
+            >
+                <Icon name="user" />
+                My Account
+            </Menu.Item>
+            <Menu.Item
+                name="Logout"
+                onClick={() => logout()}
+            >
+                <Icon name="log out" />
+                Leave
+            </Menu.Item>
+        </>
     )
 }
 
