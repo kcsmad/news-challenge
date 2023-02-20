a92c43dde6a96eecaad3c297695ba9849f1a9f3b
Talkasha <nivekzin@gmail.com>
2023-02-20 18:22:03 +0000

    feat(article): Implemented my articles page

--- src/pages/article/my-articles/my-articles-page.tsx
+++ src/pages/article/my-articles/my-articles-page.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Container} from "semantic-ui-react";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -9,6 +10,7 @@ import { Article } from "../../../types";
 const MyArticlesPage = () => {
     const [myArticles, setMyArticles] = useState([] as Article[])
     const { user } = useAuth0();
+    const navigate = useNavigate();
 
     useEffect(() => {
         setMyArticles(ArticleService.getMyArticles(user?.sub))
@@ -18,9 +20,16 @@ const MyArticlesPage = () => {
         console.log(myArticles)
     }, [myArticles])
 
+    const redirectCallback = (id: string) => (
+        navigate('/my-article/edit', { state: { articleId: id } })
+    )
+
     return (
         <Container text>
-           <ArticlesList articles={myArticles} />
+           <ArticlesList
+               articles={myArticles}
+               redirectCallback={(id: string) => redirectCallback(id)}
+           />
         </Container>
     )
 }
